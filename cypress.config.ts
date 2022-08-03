48ee73c50f71496da25f25dc0693f98a37cc4f03
Colin Ulin <colin.ulin@gocaribou.com>
2022-08-03 16:47:33 -0400

    a little more config

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,17 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
+    specPattern: 'cypress/specs/**/*.spec.ts',
+    defaultCommandTimeout: 30000,
+    chromeWebSecurity: false,
+    video: true,
+    retries: {
+        runMode: 2,
+    },
+    screenshotOnRunFailure: true,
+    fixturesFolder: false,
+    screenshotsFolder: 'cypress/artifacts/screenshots',
+    videosFolder: 'cypress/artifacts/videos',
     supportFile: false,
   },
 })
